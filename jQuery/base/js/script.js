0fa1c2cf35f9770aaf10552c3c18142ba4b7819f
Alberto Mijares Gonzalez <alberto.mijares@pop-os.localdomain>
2024-02-07 21:53:25 -0600

    lightbox

--- jQuery/base/js/script.js
+++ jQuery/base/js/script.js
@@ -1,6 +1,3 @@
-$(function() {
-  // jQuery goes here...
-
-  // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+$(function(){
+  $(".lightbox").delay(500).fadeIn(1000);
 });
